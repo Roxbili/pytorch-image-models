19cf43cfbd5ffcbf2630e8c3756bb51476456cd7
Roxbili <roxbili@163.com>
2022-03-15 16:38:10 +0800

    改回mean, std

--- train.py
+++ train.py
@@ -577,9 +577,9 @@ def main():
 
     if not args.prefetcher:     # 将数据一次性加载进内存，避免磁盘读取
         start_time = time.time()
-        _logger.info('begin load loader_train to memory')
+        _logger.info('begin ader_train to memory')
         loader_train = list(loader_train)
-        _logger.info('begin load loader_eval to memory')
+        _logger.info('begin ader_eval to memory')
         loader_eval = list(loader_eval)
         end_time = time.time()
         _logger.info('%s load loader time: %.3f' % (args.img_load, (end_time - start_time)))
